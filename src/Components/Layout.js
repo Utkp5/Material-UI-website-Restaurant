4e387675f646ac331e454cad2e95582ad621912d
PanditUtkarsh <utkp200@gmail.com>
2023-06-02 13:15:32 +0530

    changes

---
+++ src/Components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Pages/Navbar'
+
+function Layout() {
+  return (
+    <div>
+      <h1>layout</h1>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/Components/Pages/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/Navbar.css';
+
+
+function Navbar() {
+  return (
+    <div>
+      <h2>Navbar</h2>
+    </div>
+  )
+}
+
+export default Navbar
