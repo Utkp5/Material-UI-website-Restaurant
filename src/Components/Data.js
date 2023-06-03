cfc48aff1108c19a0646d71fd6ba8dd60fb09eb0
PanditUtkarsh <utkp200@gmail.com>
2023-06-03 14:06:31 +0530

    changes

---
+++ src/Components/Data.js
@@ -0,0 +1,61 @@
+import p1 from '../Images/p1.jpg'
+import p2 from '../Images/p2.jpg'
+import p3 from '../Images/p3.jpg'
+import p4 from '../Images/p4.jpg'
+import p5 from '../Images/p5.jpg'
+import p6 from '../Images/p6.jpg'
+import p7 from '../Images/p7.jpg'
+import p8 from '../Images/p8.jpg'
+
+const data = [
+    {
+        img :{p1},
+        d_name:'Italian Source Mushroom',
+        d_subName:'Mushroom / Garlic / Veggies',
+        d_price:'Rs: 350',
+    },
+    {
+        img :{p2},
+        d_name:'Fried Potatoes With Garlic',
+        d_subName:'Potatoes / Olive Oil / Garlic',
+        d_price:'RS: 799',
+    },
+    {
+        img :{p3},
+        d_name:'Tuna Roast Source',
+        d_subName:'Tuna / Potatoes / Rice',
+        d_price:'RS: 799',
+    },
+    {
+        img :{p4},
+        d_name:'Roast Pork (4 Sticks)',
+        d_subName:'Pork / Veggies / Shoyu',
+        d_price:'RS: 1799',
+    },
+    {
+        img :{p5},
+        d_name:'Salted Fried Chicken',
+        d_subName:'Chicken / Olive Oil / Salt',
+        d_price:'RS: 1799',
+    },
+    {
+        img :{p6},
+        d_name:'Crab With Curry Sources',
+        d_subName:'Crab / Potatoes / Rice',
+        d_price:'RS: 999',
+    },
+    {
+        img :{p7},
+        d_name:'Imported Salmon Steak',
+        d_subName:'Salmon / Veggies / Oil',
+        d_price:'RS: 679',
+    },
+    {
+        img :{p8},
+        d_name:'Baked Potato Pizza',
+        d_subName:'Potato / Bread / Cheese',
+        d_price:'RS: 809',
+    },
+]
+
+export default data
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Layout from '../Layout'
 import '../Styles/Home.css';
+import { Box, Button, Card, CardActionArea, CardContent, Typography } from '@mui/material';
+import h1 from '../../Images/h1.jpg'
+import h2 from '../../Images/h2.jpg'
 
 
 function Home() {
@@ -15,7 +18,69 @@ function Home() {
                  <button>ORDER NOW</button>
              </div>
          </div>
-         ok ji
+
+         {/*signature*/}
+         <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',my: 4}}>
+             <Typography sx={{fontSize:35,'@media(max-width:600px)':{fontSize:16}}}>UNTIL I DISCOVERED COOKING I WAS NEVER</Typography>
+             <Typography sx={{fontSize:35,'@media(max-width:600px)':{fontSize:16}}}>REALLY INTERESTED IN ANYTHING</Typography>
+             <Typography sx={{'@media(max-width:600px)':{fontSize:10}}}>OUR FOUNDER PANDIT UTKARSH</Typography>
+             <img className='h_img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSore5IdbEohVdtYpgEhItiIIMh2jif5TF2t6RETbfOYnQ35Gon1jezap6-Nb5aEXUBtnc&usqp=CAU" alt="signature" width={350} />
+         </Box>
+        {/*signature*/}
+
+        {/*our story*/}
+        <Box sx={{display:'flex',mx:8,alignItems:'center',mb:10,'@media(max-width:600px)':{display:'flex',flexDirection:'column'}}}>
+           <Box sx={{'@media(max-width:600px)':{display:'flex' ,flexDirection:'row'}}}>
+              <img src={h1} alt="img1" className='img_h'/>
+              <img src={h2} alt="img2" className='img_h'/>
+           </Box>
+           <Box sx={{'@media(max-width:600px)':{mt:4}}}> 
+           <Card sx={{width:480,minHeight:400,bgcolor:'inherit',display:'flex',flexDirection:'column',justifyContent:'space-evenly',
+           color:"black",borderRadius: 2,mx:-4,bgcolor:'cornsilk',p:6,
+           '@media(max-width:600px)':{width:340,height:350}}}>
+                <CardContent>
+                  <Typography sx={{ fontSize: 60, fontFamily:'Great Vibes',textAlign:'center' }} color="burlywood">Discover</Typography>
+                  <Typography sx={{ fontSize: 60, mb: 1.5,mt:-4,textAlign:'center',fontWeight:'bold',mt:-2,'@media(max-width:600px)':{fontSize:30}}}>OUR STORY</Typography>
+                  <Typography variant="body2" sx={{textAlign:'center'}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui mollitia quaerat similique unde, ducimus enim ab. Deleniti voluptates molestias illo.</Typography>
+                </CardContent>
+                <CardActionArea>
+                <Button size="small" sx={{textDecoration:"underline",mx:15,fontSize:20,color:"burlywood",
+                '@media(max-width:600px)':{mx:5.9}}}>Learn More</Button>
+                </CardActionArea>
+                </Card>
+           </Box>
+        </Box>
+        {/*our story*/}
+
+
+        <div className='hp_banner'>
+             <div className='hp_content'>
+                <h2 className='hp_h2'>Delightful</h2>
+                <h1 className='hp_h1'>EXPERIENCE</h1>
+             </div>
+         </div>
+         <Card sx={{ maxWidth: 345, marginTop: 10, marginLeft: 5 }}>
+
+      <CardActionArea>
+
+        <CardMedia component="img" height="200" alt="green iguana"
+          image="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+        />
+
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">Web Development & design</Typography>
+          <Typography variant="body2" color="text.secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, quos?</Typography>
+        </CardContent>
+
+      </CardActionArea>
+      
+      <CardActions>
+        <Button size="small" color="primary">Share</Button>
+        <Button size="small" color="primary" onClick={() => setopen(true)}>Delete</Button>
+      </CardActions>
+      
+      </Card>
+
     </Layout>
   )
 }
